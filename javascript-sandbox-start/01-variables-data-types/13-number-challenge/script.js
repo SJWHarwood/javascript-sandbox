1542566487c6582572e8abbc9cf2de7ca95ed744
simon harwood <sjwharwood@outlook.com>
2023-05-08 17:55:39 +0100

    challenge complete

---
+++ javascript-sandbox-start/01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,36 @@
+let x;
+
+x = Math.floor(Math.random() * 100 + 1);
+
+
+let y;
+
+y = Math.floor(Math.random() * 50 + 1);
+
+
+// Sum of output
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+
+// Difference of Output
+
+const difference = x - y;
+const differenceOutput = `${x} - ${y} = ${difference}`;
+
+// Product of Output
+
+const product = x * y;
+const productOutput = `${x} * ${y} = ${product}`;
+
+// Quoitent of Output
+
+const Quoitent = (x / y);
+const quotientOutput = `${x} / ${y} = ${Quoitent}`;
+
+
+// Remainder of Output
+
+const Remainder = x % y;
+const rmOutput = `${x} % ${y} = ${Remainder}`
+
+console.log(rmOutput)
