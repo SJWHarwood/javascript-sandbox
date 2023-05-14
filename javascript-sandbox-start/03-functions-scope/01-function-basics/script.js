238a79430765496cba438548fadd22e34c364ce1
simon harwood <sjwharwood@outlook.com>
2023-05-14 10:52:16 +0100

    1

---
+++ javascript-sandbox-start/03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,20 @@
+function sayHello() {
+console.log('Hello World')
+};
+
+sayHello();
+
+function add(num1,num2){
+  console.log(num1 + num2);
+};
+
+add(4,4);
+
+
+function subtract(num1, num2) {
+  return num1 - num2;
+};
+
+const answer = subtract(10,5);
+
+console.log(answer);
