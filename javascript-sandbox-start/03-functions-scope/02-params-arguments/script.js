632dcd98dcd3a9ea0f989d06f685d514bf1e7977
simon harwood <sjwharwood@outlook.com>
2023-05-14 11:24:54 +0100

    Challenge 2

---
+++ javascript-sandbox-start/03-functions-scope/02-params-arguments/script.js
@@ -0,0 +1,51 @@
+
+
+function registerUser(user = 'bot') {
+  return user + ' is registered';
+}
+
+console.log(registerUser('simon'));
+
+// Rest parameters
+
+function sum(...numbers) {
+
+  let total = 0;
+
+  for( const num of numbers) {
+    total += num;
+  }
+return total
+}
+
+console.log(sum(1,2,3));
+
+// Objects as params
+
+function loginUser(user) {
+  return `The user ${user.name} has the id of ${user.id} is logged in`;
+}
+
+const user = {
+  name: 'Simon',
+  id: 1
+};
+
+console.log(loginUser(user))
+console.log(loginUser({
+  name: 'John',
+  id: 2
+}))
+
+// Arrays as Params
+
+function randArray(...arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  const item = arr[randomIndex];
+
+  console.log(item);
+
+}
+
+randArray(1,2,3,4,5,6,7,8,9,10);
