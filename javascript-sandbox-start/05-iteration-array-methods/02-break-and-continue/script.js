7afd8eca5f2e6153ce514545eb0cca6cbebcba01
simon harwood <sjwharwood@outlook.com>
2023-07-09 14:07:37 +0100

    break and continue

---
+++ javascript-sandbox-start/05-iteration-array-methods/02-break-and-continue/script.js
@@ -0,0 +1,22 @@
+
+// Break
+
+// for (i = 0; i <= 20; i++) {
+//   if (i === 15) {
+//     console.log('breaking..');
+//     break;
+//   }
+//   console.log(i)
+// }
+
+// Continue
+
+for (i = 0; i <= 20; i++){
+
+  if (i === 13 ){
+    console.log('Skipping 13.. ');
+    continue;
+  }
+
+  console.log(i);
+}
