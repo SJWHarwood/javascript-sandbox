d3e15558ab1a04e18eaafb1fe6d74e96b1bde9c4
simon harwood <sjwharwood@outlook.com>
2023-07-23 08:24:09 +0100

    done

---
+++ javascript-sandbox-start/05-iteration-array-methods/06-for-in-loop/script.js
@@ -0,0 +1,16 @@
+// const colorObj = {
+//   color1: 'Red',
+//   color2: 'Blue',
+//   color3: 'Orange ',
+//   color4: 'Green'
+// }
+
+// for (const key in colorObj){
+//   console.log(key, colorObj[key])
+// }
+
+const arr = ['Red', 'Green','Blue', 'Yellow' ];
+
+for (const color in arr ){
+  console.log(color,arr[color])
+}
