892632bb85e7a6b5d2ffa302d3288dcb3f44db10
simon harwood <sjwharwood@outlook.com>
2023-07-25 17:08:09 +0100

    done

---
+++ javascript-sandbox-start/05-iteration-array-methods/07-forEach/script.js
@@ -0,0 +1,21 @@
+
+
+const socials = [ 'Twiiter', 'LinkedIn', 'Facebook', 'Instagram'];
+
+// socials.forEach((item, index, arr) => console.log(`${index} - ${item}`));
+
+function logSocials (social) {
+    console.log(social)
+};
+
+// socials.forEach(logSocials)
+
+
+const socialObj = [
+{ name: 'Twiiter',url: 'https://twiiter.com'},
+{ name: 'Facebook',url: 'https://Facebook.com'},
+{ name: 'Linkedin',url: 'https://linkedin.com'},
+{ name: 'instagram',url: 'https://instagram.com'}
+];
+
+socialObj.forEach((item) => console.log(item));
