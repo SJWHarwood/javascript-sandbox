52c8187e5b96439f38b784fe3dc8b7bf81cc82e7
simon harwood <sjwharwood@outlook.com>
2023-08-06 16:48:04 +0100

    done

---
+++ javascript-sandbox-start/06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,9 @@
+
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+
+// main.innerHTML = '<h1>Hello from Simon</h1>';
+
+document.querySelector('#main h1').innerText = 'Hello'
