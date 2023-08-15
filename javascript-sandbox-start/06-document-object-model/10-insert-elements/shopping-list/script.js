4173f5a3a630f9f75a3b15e320875d17a99b460e
simon harwood <sjwharwood@outlook.com>
2023-08-15 22:06:21 +0100

    Done

--- javascript-sandbox-start/06-document-object-model/10-insert-elements/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/10-insert-elements/shopping-list/script.js
@@ -1,12 +1,51 @@
 // insertAdjacentElement Example
+  function insertElement() {
+    const filter = document.querySelector('.filter');
 
+    const h1 = document.createElement('h1');
+    h1.textContent = 'insert adjacent element';
+
+    filter.insertAdjacentElement('afterEnd', h1)
+  }
 // insertAdjacentText Example
+
+function insertText() {
+
+  const item = document.querySelector('li:first-child');
+
+  item.insertAdjacentText('beforebegin', 'Hello World');
+
+}
 
 // insertAdjacentHTML example
 
+  function insertHTML() {
+    const clearBtn = document.querySelector('#clear');
+
+    clearBtn.insertAdjacentHTML('afterend', '<p>S.J.W.H</p>');
+  }
+
 // insertBefore Example
 
+function insertBeforeItem() {
+  const ul = document.querySelector('ul');
 
+  const li = document.createElement('li');
+  li.textContent = 'Insert before'
+
+  const third = document.querySelector('li:nth-child(3)');
+
+  ul.insertBefore(li, third);
+}
+
+
+insertElement();
+
+insertText();
+
+insertHTML();
+
+insertBeforeItem();
 /*
 <!-- beforebegin -->
 <p>
