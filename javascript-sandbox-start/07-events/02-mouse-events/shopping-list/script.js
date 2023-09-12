8ac21e12d99e19164ac47cf0862eb4a27e37cf40
simon harwood <sjwharwood@outlook.com>
2023-09-12 21:51:13 +0100

    done

--- javascript-sandbox-start/07-events/02-mouse-events/shopping-list/script.js
+++ javascript-sandbox-start/07-events/02-mouse-events/shopping-list/script.js
@@ -1,1 +1,8 @@
+const logo = document.querySelector('img');
 
+const onClick = () => console.log('click event');
+
+const onDoubleClick = () =>  console.log('double click')
+
+// Event Listeners
+logo.addEventListener('dblclick', onDoubleClick);
